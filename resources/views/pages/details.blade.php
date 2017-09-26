6b0048242a484e4385d95e4fdfd0f94df1cc2c7c
zenddignite <nikiviliev98@gmail.com>
2017-09-26 20:12:12 +0300

    add styles for spec layout

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -21,7 +21,16 @@
                 </div>
 
                 <div class="spec">
-
+                    <table>
+                        <tr>
+                            <th>Month</th>
+                            <th>Savings</th>
+                        </tr>
+                        <tr>
+                            <td>January</td>
+                            <td>$100</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
 
