5cd0a16a9b9f28e7ecc9402c9a54ea976a72a88c
zenddignite <nikiviliev98@gmail.com>
2017-09-30 13:09:21 +0300

    add two new sections for home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -25,58 +25,92 @@
         </div>
     </div>
 
-    <div class="feed">
+    <div class="home-feed">
         <div class="wrapper wrapper-1000">
-            <h1>Най-новите обяви за Автомобили</h1>
-            
-            <div class="news-feed">
-                <div class="item">
-                    <div class="image">
-                        <img src="images/example-car.png" alt="example">
+            <div class="flex-layout">
+                <div class="offer">
+                    <h2>Какво предлагаме?</h2>
+    
+                    <div class="head">
+                        <div class="image">
+                            <img src="images/keys.jpg" alt="keys">
+                        </div>
+    
+                        <div class="links">
+                            <a href="#">Автомобили</a>
+                        </div>
                     </div>
     
-                    <div class="news-content">
-                        <h2>Tesla Model S</h2>
-                        
-                        <h3 class="price">100 000€</h3>
-                        <p class="miles">11 931</p>
-                        <p class="city">Varna</p>
+                    <div class="our-offers">
+                        <h2>Вижте нашите оферти в :</h2>
+    
+                        <div class="external-sites">
+                            <a href="http://emoauto.mobile.bg/">
+                                <img src="images/logo_mobilebg.jpg" alt="logo_mobilebg">
+                            </a>
+    
+                            <a href="http://emoauto.cars.bg/">
+                                <img src="images/logo_carsbg.jpg" alt="logo_carsbg">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="feed">
+                    <h1>Най-новите обяви за Автомобили</h1>
+    
+                    <div class="news-feed">
+                        <div class="item">
+                            <div class="image">
+                                <img src="images/example-car.png" alt="example">
+                            </div>
             
-                        <span class="date">01/01/17</span>
+                            <div class="news-content">
+                                <h2>Tesla Model S</h2>
+                                
+                                <h3 class="price">100 000€</h3>
+                                <p class="miles">11 931</p>
+                                <p class="city">Varna</p>
+                    
+                                <span class="date">01/01/17</span>
+                            </div>
+                        </div>
+        
+                        <div class="item">
+                            <div class="image">
+                                <img src="images/example-car.png" alt="example">
+                            </div>
+            
+                            <div class="news-content">
+                                <h2>Tesla Model S</h2>
+                                
+                                <h3 class="price">100 000€</h3>
+                                <p class="miles">11 931</p>
+                                <p class="city">Varna</p>
+                    
+                                <span class="date">01/01/17</span>
+                            </div>
+                        </div>
+        
+                        <div class="item">
+                            <div class="image">
+                                <img src="images/example-car.png" alt="example">
+                            </div>
+            
+                            <div class="news-content">
+                                <h2>Tesla Model S</h2>
+                                
+                                <h3 class="price">100 000€</h3>
+                                <p class="miles">11 931</p>
+                                <p class="city">Varna</p>
+                    
+                                <span class="date">01/01/17</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="item">
-                    <div class="image">
-                        <img src="images/example-car.png" alt="example">
-                    </div>
-    
-                    <div class="news-content">
-                        <h2>Tesla Model S</h2>
-                        
-                        <h3 class="price">100 000€</h3>
-                        <p class="miles">11 931</p>
-                        <p class="city">Varna</p>
-            
-                        <span class="date">01/01/17</span>
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="image">
-                        <img src="images/example-car.png" alt="example">
-                    </div>
-    
-                    <div class="news-content">
-                        <h2>Tesla Model S</h2>
-                        
-                        <h3 class="price">100 000€</h3>
-                        <p class="miles">11 931</p>
-                        <p class="city">Varna</p>
-            
-                        <span class="date">01/01/17</span>
-                    </div>
-                </div>
+                <div class="other"></div>
             </div>
         </div>
     </div>
