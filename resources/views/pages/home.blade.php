534ffa7040beeb478e045f5ca098cc1e5c0978fc
zenddignite <nikiviliev98@gmail.com>
2017-10-28 14:21:27 +0300

    add styles to the scss

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -28,10 +28,10 @@
     <div class="home-feed">
         <div class="wrapper wrapper-1000">
             <div class="flex-layout">
-                <div class="offer" style="width:25%; border-radius:10px">
+                <div class="offer">
                     <h3>Какво предлагаме?</h3>
     
-                    <div class="head">
+                    <div class="flex">
                         <div class="image_2">
                             <img src="images/keys.jpg" alt="keys">
                         </div>
@@ -43,9 +43,9 @@
                     </div>
     
                     <div class="our-offers">
-                        <h4 style="margin-top:10px">Вижте нашите оферти в :</h4>
+                        <h4>Вижте нашите оферти в :</h4>
     
-                        <div class="external-sites">
+                        <div class="external-sites flex">
                             <a href="http://emoauto.mobile.bg/">
                                 <img src="images/logo_mobilebg.jpg" alt="logo_mobilebg">
                             </a>
@@ -54,21 +54,22 @@
                                 <img src="images/logo_carsbg.jpg" alt="logo_carsbg">
                             </a>
                         </div>
-                        <h4 style="margin-top:10px">Кандидатствайте за лизинг:</h4>
+
+                        <h4>Кандидатствайте за лизинг:</h4>
     
-                        <div class="external-sites">
+                        <div class="external-sites flex">
                             <a href="https://www.mogo.bg/">
-                                <img src="images/mogo_logo.png" alt="logo_mobilebg" width="35%">
+                                <img src="images/mogo_logo.png" alt="logo_mobilebg" >
                             </a>
     
                             <a href="http://www.bnpparibas-pf.bg/">
-                                <img src="images/pariba.png" alt="logo_carsbg" width="35%">
+                                <img src="images/pariba.png" alt="logo_carsbg">
                             </a>
                         </div>
                     </div>
                 </div>
     
-                <div class="feed" style="width:33%; border-radius:10px">
+                <div class="feed">
                     <h3>Най-новите обяви:</h3>
     
                     <div class="news-feed">
@@ -90,8 +91,14 @@
                     </div>
                 </div>
 
-                <div class="other" style="width:40%; border-radius:10px">
-                    <iframe style="width:100%; border-radius:10px 10px 0 0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2073.2185403970993!2d23.22879762936387!3d42.70427600391737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa856e3cd80c23%3A0x9c25942e7766ab86!2z0JXQnNCeINCQ0KPQotCe!5e0!3m2!1sbg!2sbg!4v1509175666069" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <div class="other">
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2073.2185403970993!2d23.22879762936387!3d42.70427600391737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa856e3cd80c23%3A0x9c25942e7766ab86!2z0JXQnNCeINCQ0KPQotCe!5e0!3m2!1sbg!2sbg!4v1509175666069" 
+                        width="600" 
+                        height="450" 
+                        frameborder="0" 
+                        border="0"
+                        allowfullscreen></iframe>
                 </div>
             </div>
         </div>
